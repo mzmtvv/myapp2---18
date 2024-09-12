5a531e81fdb95ec0d9c6b7096bba042473d462d4
Kaiwit Ngamrabiab <mzmtvth@gmail.com>
2024-09-12 14:19:59 +0700

    first commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,30 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { Component } from '@angular/core';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+
+export const routes: Routes = [
+ {
+    path:'home',
+    component: HomeComponent
+ },
+ {
+    path:'about',
+    component: AboutComponent
+ },
+ {
+    path:'contact',
+    component: ContactComponent
+ },
+ {
+    path:'login',
+    component: LoginComponent
+ },
+ {
+    path:'register',
+    component: RegisterComponent
+ },
+];
